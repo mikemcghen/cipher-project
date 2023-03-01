adf9aa3c7f79fd751bb375a8ae89f66e4ec493e1
mikemcghen <66638976+mikemcghen@users.noreply.github.com>
2023-03-01 10:27:08 -0500

    Add files via upload

---
+++ Passion Projects/cipher-project/cipher-passion-project/cipher-passion-project/KeywordCipher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cipher_passion_project
+{
+    public class KeywordCipher
+    {
+
+    }
+}
---
+++ Passion Projects/cipher-project/cipher-passion-project/cipher-passion-project/UI.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cipher_passion_project
+{
+    internal class UI
+    {
+    }
+}
